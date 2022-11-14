055b669041e0621b1a814fe9c49e944268e8dad5
NiteChill <puissantachille@gmail.com>
2022-11-14 23:08:18 +0100

    délais loading screen

---
+++ src/component/home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return(
+    `
+    <div class="home-screen">
+    </div>
+    `
+    )
+}
+
+export default Home ;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import "./default.scss"
 import Loading from "./component/loading"
+import Home from "./component/home"
 const app = document.getElementById('app');
-
 let content = "";
 content += Loading();
+content += Home();
 app.innerHTML = content;
+const loadingScreen = document.querySelector('.loading-screen');
+let waitingLoading = window.setTimeout(loadingAnimation, 1500);
+
+function loadingAnimation(){
+    loadingScreen.style.marginLeft = '-100%';
+}
